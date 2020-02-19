e961ad764967dcb1e1618b8084bb981cd53932ac
weikee <weikee070194@gmail.com>
2020-02-19 17:56:13 +0800

    add reactdom hydrate

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,7 @@
-console.log("Hi there");
+// start point for client side application
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+
+ReactDOM.hydrate(<Home></Home>, document.querySelector("#root"));
